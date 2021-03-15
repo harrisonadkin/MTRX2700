81b0584e914ee52139b27d6b10aa7419b9967ded
Harrison Adkin <harrison.adkin7@gmail.com>
2021-03-16 10:20:22 +1100

    Added comments

--- uppercase_main.asm
+++ uppercase_main.asm
@@ -24,10 +24,10 @@ ROMStart    EQU  $4000  ; absolute address to place my code/constant data
  ; Insert here your data definition.
 Counter     DS.W 1
 FiboRes     DS.W 1
-String:     FCC "ALL. lower"
-            FCB $00
-UpperVal:      EQU #$5F
-LowerVal:      EQU #$20
+String:     FCC "ALL. lower"  ; stores string
+            FCB $00 ; appends end of string with 0 character
+UpperVal:   EQU #$5F ; stores 0101 1111 binary
+LowerVal:   EQU #$20 ; stores 0010 0000 binary
 
 
 ; code section
@@ -44,27 +44,27 @@ _Startup:
 
 
 mainLoop:
-            LDX #String
-innerLoop:  LDAA 1, X+
-            BEQ mainLoop
-            CMPA #97
-            BHS upperLoop
-            CMPA #90
-            BLS parsing
+            LDX #String ; begin by loading memory address at label string
+innerLoop:  LDAA 1, X+  ; load first character in string into A, and increment
+            BEQ mainLoop  ; break if 0 to stop looping at end
+            CMPA #97  ; compare A to 97 to see if capital / lowercase
+            BHS upperLoop ; break if ___ to uppercase loop
+            CMPA #90  ; compare A to 90 to see if capital / lowercase
+            BLS parsing ; break to parsing if ___
 
 
 upperLoop:
             STAA $2010
-            bra innerLoop
+            BRA innerLoop ; break back to look at next character
 
 parsing:
-            CMPA #65
-            BHS lowerLoop
-            bra innerLoop
+            CMPA #65  ; compare to 65 to find if letter, space or fullstop
+            BHS lowerLoop ; break if ___ to lower
+            BRA innerLoop ; otherwise break back to inner to look at next character
 
 lowerLoop:
             STAA $3010
-            bra innerLoop
+            BRA innerLoop ; break back to look at next character
 
 
 
