fc3f778d60ddf3f0f3cc1f76e4e32a2f64ee20c0
Harrison Adkin <harrison.adkin7@gmail.com>
2021-03-23 16:53:20 +1100

    cleaning up my bad commits

--- exercise_2.asm
+++ exercise_2.asm
@@ -41,7 +41,7 @@ NINE:       EQU $6F
 ZERO:       EQU $3F
 FLAG:       EQU $03
 
- 
+
 ; code section
             ORG   ROMStart
 
@@ -54,112 +54,109 @@ _Startup:
 
 
 
-always:
-            LDX #String
-
 mainLoop:
-            
+            LDX #String
             LDY #Digits
-            
-                        
+
+
             MOVB #$0F,DDRP               ; set ddrp to 15 ????? (output port P - 7 seg)
             MOVB #$FF,DDRB               ; set ddrn to 255
             MOVB #$00,DDRH               ; set push button for input
             MOVB #$FF,PTP
 
-            
-            
+
+
             LDAA #FLAG
             CMPA #$01
             BEQ readButton
             CMPA #$02
-            BEQ drawString              
-            CMPA #$03               
+            BEQ drawString
+            CMPA #$03
             BEQ numScroll
-            
-            
-            
-  readButton:           
+
+
+
+  readButton:
             BCLR PTP, $01           ; which 7Seg 01 is 1, 02 is 2, 04 is 3, 08 is 4
             LDAA PTH
             CMPA #00
             BEQ mappingFunction
             BRA readButton
-  
-  
+
+
   drawString:
             LDAA 1,Y+
             STAA PTP
             BRA mappingFunction
-           
-            
+
+
   numScroll:
             LDAA 1,Y+
             BEQ mainLoop
             STAA PTP
-            BRA mappingFunction       
-                                                        
-                       
-            
+            BRA mappingFunction
+
+
+
   mappingFunction:
             LDAA 1, X+
             BEQ mainLoop
-            
+
             LDAB #ONE
             CMPA #$31
             BEQ writeHex
-            
+
             LDAB #TWO
             CMPA #$32
             BEQ writeHex
-            
+
             LDAB #THREE
             CMPA #$33
             BEQ writeHex
-            
+
             LDAB #FOUR
             CMPA #$34
             BEQ writeHex
-            
+
             LDAB #FIVE
             CMPA #$35
             BEQ writeHex
-            
+
             LDAB #SIX
             CMPA #$36
             BEQ writeHex
-            
+
             LDAB #SEVEN
             CMPA #$37
             BEQ writeHex
-            
+
             LDAB #EIGHT
             CMPA #$38
             BEQ writeHex
-            
+
             LDAB #NINE
             CMPA #$39
             BEQ writeHex
-            
+
             LDAB #ZERO
             CMPA #$30
             BEQ writeHex
-            
+
             BRA mappingFunction   ; note this loops if a letter not to display it (important for ex 4)
-      
-           
-                 
+
+
+
   writeHex:
            STAB PORTB
            LDAB #20
            PSHX
            PSHY
            BRA longDelay ; short delay comment out DBNE B
-           
-           
+
+
   longDelay:
            LDY #6000
-           
+
       shortDelay:
            PSHA
            PULA
@@ -171,16 +168,16 @@ mainLoop:
            PULA
            DBNE Y,shortDelay
            DBNE B,longDelay
-   
+
    PULY
    PULX
-   
+
    LDAA #FLAG
    CMPA #$01
    BEQ readButton
    CMPA #$02
-   BEQ drawString                 
-   CMPA #$03                       
+   BEQ drawString
+   CMPA #$03
    BEQ numScroll
 
 
@@ -192,4 +189,3 @@ mainLoop:
 ;**************************************************************
             ORG   $FFFE
             DC.W  Entry           ; Reset Vector
- 
