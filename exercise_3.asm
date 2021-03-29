66874e973322993541113664bdcabc1876a1f8a2
Harrison Adkin <harrison.adkin7@gmail.com>
2021-03-29 22:19:21 +1100

    finish ex3

--- exercise_3.asm
+++ exercise_3.asm
@@ -26,10 +26,12 @@ Counter     DS.W 1
 FiboRes     DS.W 1
 
  ; Our constants.
-String:     FCC "Thanks for making Tron FUN !! :) ~12345 "
-            FCB $0D
-            FCB $00
-Flag:       EQU $01
+String:       FCC "Thanks for making Tron FUN !! :) ~12345 "
+              FCB $0D
+              FCB $0A
+              FCB $00
+FLAG:         EQU $03
+InputString:  RMB 128
 
 
 ; code boi
@@ -41,50 +43,80 @@ Entry:
             LDS #RAMEnd+1
             CLI
 
-
 mainLoop:
-        LDX #String
-        MOVB #$00,SCI1BDH
-        MOVB #$9C,SCI1BDL
-        MOVB #$4C,SCI1CR1
-        MOVB #$0C,SCI1CR2
-        MOVB #$00,SCI1DRL
-        LDAA #Flag
-        CMPA $01
-        BEQ sendString
-        CMPA $02
-        BEQ receiveString
 
+        MOVB #$00,SCI1BDH  ; baud rate
+        MOVB #$9C,SCI1BDL  ; baud rate 156 decimal
+        MOVB #$00,SCI1CR1  ; always 0
+        MOVB #$00,SCI1DRL
+        LDAA #FLAG
+        LDX #String
+        CMPA #$01
+        BEQ sendString
+        LDX #InputString
+        CMPA #$02
+        BEQ receiveString
+        CMPA #$03
+        BEQ readAndWriteString
+
+
+;**************************************************************
+;*                       Functions                            *
+;**************************************************************
 
 sendString:
         LDAA 1, X+
         BEQ mainLoop
-        BRA outputChar
+        MOVB #mSCI1CR2_TE,SCI1CR2     ; mSCI1CR2_TE  // $0C
+        BRCLR SCI1SR1,mSCI1SR1_TDRE,*
+        STAA SCI1DRL
+        LDAB #17 ; should be 17.5 but round down from random overflow
+        JSR delay
+        BRA sendString
 
 receiveString:
-        LDAA 1, X+
+        MOVB #mSCI1CR2_RE,SCI1CR2 ; bit mask to read
+        BRCLR SCI1SR1, mSCI1SR1_RDRF,*
+        LDAA SCI1DRL
+        STAA 1, X+
+        CMPA #$0D
         BEQ mainLoop
-        BRA inputChar
+        BRA receiveString
 
-;sendBreak:
- ;       BSET SCI1CR2,mSCI1CR2_SBK
-  ;      JSR delay
-   ;     BCLR SCI1CR2,mSCI1CR2_SBK
-    ;    RTS
+readAndWriteString:
+        MOVB #mSCI1CR2_RE,SCI1CR2 ; bit mask to read
+        BRCLR SCI1SR1, mSCI1SR1_RDRF,*
+        LDAA SCI1DRL
+        STAA 1, X+
+        CMPA #$0D
+        BEQ parsing
+        BRA readAndWriteString
 
-outputChar:
-        BRCLR SCI1SR1,mSCI1SR1_TDRE,*
-        STAA  SCI1DRL
-        LDAB #17
-        BRA delay
+    parsing:
+          LDAA #$0A
+          STAA 1, X+
+          LDAA #$00
+          STAA 1, X+
+          LDX #InputString
+
+    beginWrite:
+          MOVB #mSCI1CR2_TE,SCI1CR2  ; bitmask to write
+          BRCLR SCI1SR1,mSCI1SR1_TDRE,*
+          LDAA 1, X+
+          BEQ mainLoop
+
+          STAA SCI1DRL
+          LDAB #17 ; should be 17.5 but round down from random overflow
+          JSR delay
+          BRA beginWrite
 
 
-inputChar:
-       ; solve logic
-
+;**************************************************************
+;*                         Delays                             *
+;**************************************************************
 
 delay:
-        LDY #60000
+        LDY #60000 ;set to 60000 for 1s
 
    delayLoop:
           PSHA
@@ -98,10 +130,7 @@ delay:
           DBNE Y,delayLoop
           DBNE B,delay
 
- BRA sendString
-
-
- 
+ RTS
 
 
 ;**************************************************************
